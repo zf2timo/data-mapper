4d44563e90f533389e5772b2f4ffb7bf22361926
zf2timo <tewes@henworx.de>
2014-04-28 21:53:05 +0200

    Added Autoload configuration to Module\Module

--- src/Module/Module.php
+++ src/Module/Module.php
@@ -4,9 +4,10 @@
 namespace Module;
 
 
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
-class Module implements ServiceProviderInterface
+class Module implements ServiceProviderInterface, AutoloaderProviderInterface
 {
 
     /**
@@ -18,5 +19,22 @@ class Module implements ServiceProviderInterface
     public function getServiceConfig()
     {
         return include __DIR__ . '/../../config/service.config.php';
+    }
+
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
+                ),
+            ),
+        );
     }
 }
